02a9d37305e9bda2fc601a2dcf7fbea5b391411a
Shivprasad Nikam <116971586+shivprasadnikam@users.noreply.github.com>
2023-01-30 11:29:43 +0530

    Create solidRectangle.cpp

---
+++ solidRectangle.cpp
@@ -0,0 +1,27 @@
+    /*
+	    Solid rectagle pattern
+	    
+	          * * * *
+	          * * * *
+	          * * * *
+	
+	*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	for(int i=0; i<3; i++)
+	{
+	    for(int j=0; j<4; j++)
+	    {
+	        cout<<"* ";
+	        
+	    }
+	    std::cout << "\n" << std::endl;
+	}
+	return 0;
+}
